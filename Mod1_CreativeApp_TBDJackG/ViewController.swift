2d8b05979e60c2ac83c591897667479aa3ff61b9
Jack Gronberg <Jackgronberg@gmail.com>
2020-09-14 09:21:35 -0500

    it's too ez

--- Mod1_CreativeApp_TBDJackG/ViewController.swift
+++ Mod1_CreativeApp_TBDJackG/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    var counter = 10
+    var clicks=0
+    var OurTimer = Timer()
+    var highscore = 0
+    @IBOutlet weak var clickerButton: UIButton!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     super.viewDidLoad()
+      
+   clickerButton.isHidden = true
+ }
+    
+
+
+
+ @objc func updateCounter() {
+     //example functionality
+     if counter >= 0 {
+        if counter <= 10{
+        label.text  = String("\(counter)")
+         counter -= 1
+     }
     }
-
-
+ }
+      
+    @IBAction func startButton(_ sender: Any) {
+        clickerButton.isHidden=false
+        rulesLabel.isHidden=true
+        OurTimer.invalidate()
+        OurTimer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        counter=10
+        clicks=0
+        scoreLabel.text=String("Score:   \(clicks)")
+    }
+ 
+    @IBAction func clickerButton(_ sender: Any) {
+        if(counter>0){
+        clicks+=1
+       // highscore+=1
+    }
+        scoreLabel.text = "Score:  \(clicks)"
+        if(clicks>=highscore){
+            highscore=clicks
+           highscoreLabel.text = "Highscore: \(highscore)"
+        }
+    }
+    
 }
 
